1ded50cde370d93c90d839c8da37c44830c0b38d
DataSmithVa <154573652+DataSmithVa@users.noreply.github.com>
2024-07-11 01:01:16 -0400

    updates to nav links

--- app/ui/nav_links.tsx
+++ app/ui/nav_links.tsx
@@ -2,10 +2,11 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/public/CIS_LOGO.svg";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import { ContactButton } from "./buttons";
+import logo from "@/public/CIS_LOGO.svg";
+import menu from "@/public/menu.svg";
 
 // Array of links to display in the navigation.
 const links = [
@@ -15,10 +16,15 @@ const links = [
   { name: "Contact Us", href: "/contact" },
 ];
 
+// Open hamburger menu
+const openMenu = (e) => {
+  console.log("open menu fired");
+};
+
 export default function NavLinks() {
   const pathname = usePathname();
   return (
-    <div className="container mx-auto my-4 flex justify-between">
+    <div className="container mx-4 my-8 flex justify-between p-4 md:mx-auto">
       <Image src={logo} alt="Logo" className="h-12 w-auto" priority />
       <div className="mr-8 grid grid-flow-col items-center gap-4 text-center">
         {links.map((link) => {
@@ -28,7 +34,7 @@ export default function NavLinks() {
                 key={link.name}
                 href={link.href}
                 className={clsx(
-                  "rounded-full border-2 border-slate-500 bg-slate-200 px-4 py-1 font-poppins text-slate-500 hover:bg-slate-500 hover:text-slate-200",
+                  "hidden rounded-full border-2 border-slate-500 bg-slate-200 px-4 py-1 font-poppins text-slate-500 hover:bg-slate-500 hover:text-slate-200 md:block",
                   {
                     "text-slate-800": pathname === link.href,
                   },
@@ -51,6 +57,12 @@ export default function NavLinks() {
             );
           }
         })}
+        <Image
+          src={menu}
+          alt="Hamburger Menu"
+          className="h-10 w-auto md:hidden"
+          onClick={openMenu}
+        />
       </div>
     </div>
   );
