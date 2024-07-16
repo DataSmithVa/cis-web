77b36ce638c2e2333026e726ecd4486fabeac077
DataSmithVa <154573652+DataSmithVa@users.noreply.github.com>
2024-07-16 00:30:18 -0400

    Added hero component // Issues with tailwinds background image for hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "./ui/hero/hero";
 import UnderMaint from "./ui/maintenence/under_maint";
 import NavLinks from "./ui/nav_links";
 
 export default function Home() {
   return (
     <div>
+      <Hero />
       <UnderMaint />
     </div>
   );
